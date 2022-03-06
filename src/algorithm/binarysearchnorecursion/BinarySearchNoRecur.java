db94884926963461275e6aac8fc97b34a50f89ad
rotion <18112357749@163.com>
2022-03-06 18:51:27 +0800

    second commit!

--- src/algorithm/binarysearchnorecursion/BinarySearchNoRecur.java
+++ src/algorithm/binarysearchnorecursion/BinarySearchNoRecur.java
@@ -9,6 +9,7 @@ public class BinarySearchNoRecur{
     }
 
     public static int binarySearch(int[] arr, int target) {
+        int add;
         int left = 0;
         int right = arr.length-1;
         while (left <= right) {
